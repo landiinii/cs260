6090cafba2874519db0576394c13bfe503a7778a
landiinii <bailey.landeng@gmail.com>
2020-11-28 01:41:25 -0700

    finished weather

---
+++ weather/js/script.js
@@ -0,0 +1,68 @@
+document.getElementById("weatherSubmit").addEventListener("click", function(event) {
+  event.preventDefault();
+  const value = document.getElementById("weatherInput").value;
+  if (value === "")
+    return;
+  console.log(value);
+  const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=476402509737649b508ae10ab8f35cbf";
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      console.log(json);
+      let results = "";
+      results += '<h2>Current ' + json.name + " Weather:</h2>";
+      document.getElementById("weatherResultsTitle").innerHTML = results;
+      results = "";
+      for (let i=0; i < json.weather.length; i++) {
+	        results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+      }
+      results += '<h4>It is ';
+      for (let i=0; i < json.weather.length; i++) {
+	        results += json.weather[i].description
+	        if (i !== json.weather.length - 1)
+	           results += ", "
+          if (i===json.weather.length -2){
+            results += "and ";
+          }
+      }
+      results += " with a temperature of " + Math.round(json.main.temp) + " &deg;F</h4>"
+      document.getElementById("weatherResultsInfo").innerHTML = results;
+
+      const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=476402509737649b508ae10ab8f35cbf";
+      fetch(url2)
+        .then(function(response) {
+          return response.json();
+        }).then(function(json) {
+          console.log(json);
+          let days = {};
+          for (let i=0; i < json.list.length; i++) {
+            if(moment(json.list[i].dt_txt).format('MMM Do') in days){
+              days[moment(json.list[i].dt_txt).format('MMM Do')].push(json.list[i]);
+            }
+            else{
+              days[moment(json.list[i].dt_txt).format('MMM Do')] = [json.list[i]];
+            }
+          }
+          console.log(days);
+          let forecast = "<h2>Forecast: </h2> <div class=\"grid\">";
+          for (date in days) {
+            forecast += "<div class=\"grid-item\">";
+            forecast += "<h4>" + moment(days[date][0].dt_txt).format('dddd, MMM Do') + "</h4>";
+            for(let j=0; j<days[date].length; j++){
+              forecast += "<div class=\"weather-time\">";
+            	forecast += "<p>" + moment(days[date][j].dt_txt).format('h a') + "</p>";
+              forecast += '<img src="http://openweathermap.org/img/w/' + days[date][j].weather[0].icon + '.png"/>';
+            	forecast += "<p>" + Math.round(days[date][j].main.temp) + "&deg;F</p>";
+              forecast += "</div>"
+              if(j !== days[date].length-1){
+                forecast += "<hr>";
+              }
+            }
+            forecast += "</div>"
+          }
+          forecast += "</div>"
+          document.getElementById("forecastResults").innerHTML = forecast;
+        });
+    });
+});
